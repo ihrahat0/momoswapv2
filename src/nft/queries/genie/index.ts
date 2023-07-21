4fd38703c9d83c750c9739e930ef9a0ff330fbd6
ihrahat0 <136366505+ihrahat0@users.noreply.github.com>
2023-07-21 14:27:37 +0600

    first commit

--- src/nft/queries/genie/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ActivityFetcher'
-export * from './CollectionPreviewFetcher'
-export * from './RouteFetcher'
-export * from './SearchCollectionsFetcher'
-export * from './TrendingCollectionsFetcher'
--- src/utils/addressesAreEquivalent.ts
+++
@@ -1,4 +0,0 @@
-export function addressesAreEquivalent(a?: string, b?: string) {
-  if (!a || !b) return false
-  return a === b || a.toLowerCase() === b.toLowerCase()
-}
